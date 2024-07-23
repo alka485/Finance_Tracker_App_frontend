bf1e3db0fbec6def63e9f7917d714ead2c92d2d2
Alka Sah <alkasah485@gmail.com>
2024-07-23 13:11:04 -0700

    modification saved

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
+//-------------------------dependency
+import {avatar}  from '../img/avatar.png'
+
+const NavStyled = styled.div`
+      padding: 2rem 1.5rem;
+      width: 374px;
+      height: 100%;
+      background: rgba(252,246,249,0.78);
+      border: 3px solid #FFFFFF;
+      backdrop-filter: blur(4.5px);
+      border-radius: 32px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      gap: 2rem;
+      
+`;
+
+const Navigation = () => {
+  return (
+    <NavStyled>
+        <div className="user-con">
+            <img src={avatar} alt=''/>
+            <div className="text">
+                <h2>Mike</h2>
+                <p>Your Money</p>
+            </div>
+        </div>
+    </NavStyled>
+  )
+}
+
+export default Navigation
