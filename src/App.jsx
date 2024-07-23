4995dce970d4a0549e56ecd64e4cd060377f40af
Alka Sah <alkasah485@gmail.com>
2024-07-23 11:20:45 -0700

    modification saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import styled from 'styled-components'
+import bg from './img/bg.png'
 
 import './App.css'
+
+const AppStyled = styled.div `
+      height: 100vh;
+      background-image: url(${bg});
+      position:relative;
+`;
 
 function App() {
   
 
   return (
-    <>
+    <AppStyled className='App'>
       
-    </>
+    </AppStyled>
   )
 }
 
