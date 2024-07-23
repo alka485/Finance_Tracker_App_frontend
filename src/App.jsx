45a735ab817d212304889eeebf1c7a761bf7030e
Alka Sah <alkasah485@gmail.com>
2024-07-23 14:14:00 -0700

    modification saved

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,27 @@ const AppStyled = styled.div `
       height: 100vh;
       background-image: url(${bg});
       position:relative;
+      main{
+          flex: 1;
+          background: rgba(252, 246, 249, 0.78);
+          border: 3px solid #FFFFFF;
+          backdrop-filter: blur(4.5px);
+          border-radius: 32px;
+          overflow-x: hidden;
+          &::-webkit-scrollbar{
+            width: 0;
+          }
+        }
 `;
 
 function App() {
+
   const[active, setActive] = useState(1);
+
+  //func displayData
+  const displayData = () => {
+    console.log("hello");
+  }
   
 
   return (
@@ -24,6 +41,9 @@ function App() {
       {/* <Orb/> */}
       <MainLayout>
         <Navigation active={active} setActive={setActive}/>
+        <main>
+          {displayData()}
+        </main>
         
       </MainLayout>
       
