57a0d8419f3a3f1f9fead0355087ab65c7b00505
Alka Sah <alkasah485@gmail.com>
2024-07-23 15:21:45 -0700

    modification saved

--- src/context/globalContext.jsx
+++ src/context/globalContext.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React , {useState} from 'react'
 import axios from 'axios'
 
 //BASEURL
+const BASE_URL = "http://localhost:3008/api/v1";
 
-const BASE_URL = "http://localhost:3008/api/v1"
 const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children}) => {
+
+    const[incomes, setIncomes] = useState([]);
+    const[expenses, setExpenses] = useState([]);
+    const[error, setError] = useState(null);
     return<GlobalContext.Provider>
         {children}
     </GlobalContext.Provider>
