fde92e8e71933e7f028cae29bbaa4a02d204e7b0
Alka Sah <alkasah485@gmail.com>
2024-07-24 09:47:24 -0700

    modification saved

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React  from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 
 const FormStyled = styled.form ``;
 
+
 const Form = () => {
+
+    //by default value is empty
+    const [ inputState , setInputState] = useState({
+        title : '',
+        amount : '',
+        date : '',
+        category : '',
+        description : ''
+    
+    });
+
+    //destructure the value
+    const { title ,amount,date,category,description} = inputState;
+    
   return (
     <FormStyled>
         <div className="input-control">
