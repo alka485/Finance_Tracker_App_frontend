8d1e2071c587cf583edc8ece377b1cace5cc0165
Alka Sah <alkasah485@gmail.com>
2024-07-25 00:20:32 -0700

    modification saved

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -8,7 +8,36 @@ import { useGlobalContext } from '../context/globalContext';
 import Form from './Form';
 import IncomeItem from './IncomeItem';
 
-const IncomeStyled = styled.div ``;
+const IncomeStyled = styled.div `
+          display: flex;
+          overflow: auto;
+          .total-income{
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              background: #FCF6F9;
+              border: 2px solid #FFFFFF;
+              box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+              border-radius: 20px;
+              padding: 1rem;
+              margin: 1rem 0;
+              font-size: 2rem;
+              gap: .5rem;
+              span{
+                  font-size: 2.5rem;
+                  font-weight: 800;
+                  color: var(--color-green);
+              }
+          }
+          .income-content{
+              display: flex;
+              gap: 2rem;
+              .incomes{
+                  flex: 1;
+              }
+          }
+
+`;
 
 const Income = () => {
   const {addIncome,incomes,getIncomes} = useGlobalContext();
@@ -38,8 +67,7 @@ const Income = () => {
                         date={date} 
                         type={type}
                         category={category} 
-                        indicatorColor="var(--color-green)"
-              
+                        indicatorColor="var(--color-green)"          
               />
             })}
           </div>
