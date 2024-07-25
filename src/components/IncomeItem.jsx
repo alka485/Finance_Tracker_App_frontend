bd3173a701e4ccff1824a2fc2b6b2bd2d5c0550a
Alka Sah <alkasah485@gmail.com>
2024-07-24 22:20:31 -0700

    modification saved

--- src/components/IncomeItem.jsx
+++ src/components/IncomeItem.jsx
@@ -7,7 +7,52 @@ import { bitcoin, book, calender, card, circle, clothing, comment, dollar, food,
 import Button from './Button';
 
 
-const IncomeItemStyled = styled.div``;
+const IncomeItemStyled = styled.div`
+            background: #FCF6F9;
+            border: 2px solid #FFFFFF;
+            box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+            border-radius: 20px;
+            padding: 1rem;
+            margin-bottom: 1rem;
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+            width: 100%;
+            color: #222260;  
+            .icon{
+                width: 80px;
+                height: 80px;
+                border-radius: 20px;
+                background: #F5F5F5;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border: 2px solid #FFFFFF;
+                i{
+                    font-size: 2.6rem;
+                }
+             }
+            .content{
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                gap: .2rem;
+                    h5{
+                        font-size: 1.3rem;
+                        padding-left: 2rem;
+                        position: relative;
+                        &::before{
+                            content: '';
+                            position: absolute;
+                            left: 0;
+                            top: 50%;
+                            transform: translateY(-50%);
+                            width: .8rem;
+                            height: .8rem;
+                            border-radius: 50%;
+                            background: ${props => props.indicator};
+                        }           
+ `;
 
 const IncomeItem = ({
     id,
@@ -21,7 +66,7 @@ const IncomeItem = ({
     type
 }) => {
   return (
-    <IncomeItemStyled>
+    <IncomeItemStyled indicator={indicatorColor}>
         <div className="icon"></div>
         <div className="content">
             <h5>{title}</h5>
