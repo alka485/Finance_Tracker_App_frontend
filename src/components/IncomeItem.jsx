51f9f2625789d41ac005da37dd4632af04e90c2b
Alka Sah <alkasah485@gmail.com>
2024-07-24 22:12:23 -0700

    modification saved

--- src/components/IncomeItem.jsx
+++ src/components/IncomeItem.jsx
@@ -1,9 +1,35 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import styled from 'styled-components'
+//------------------------------------------dependency
+import { bitcoin, book, calender, card, circle, clothing, comment, dollar, food, freelance, medical, money, piggy, stocks, takeaway, trash, tv, users, yt } from '../utils/Icons';
 
-const IncomeItem = () => {
+
+const IncomeItemStyled = styled.div``;
+
+const IncomeItem = ({
+    id,
+    title,
+    amount,
+    date,
+    category,
+    description,
+    deleteItem,
+    indicatorColor,
+    type
+}) => {
   return (
-    <div>IncomeItem</div>
+    <IncomeItemStyled>
+        <div className="icon"></div>
+        <div className="content">
+            <h5>{title}</h5>
+            <div className="inner-content">
+                <div className="text">
+                    <p>{dollar}</p>
+                </div>
+            </div>
+        </div>
+    </IncomeItemStyled>
   )
 }
 
