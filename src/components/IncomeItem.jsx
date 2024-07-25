fca279297eef30e077f1e24674be0250e83da311
Alka Sah <alkasah485@gmail.com>
2024-07-25 12:17:19 -0700

    modification saved

--- src/components/IncomeItem.jsx
+++ src/components/IncomeItem.jsx
@@ -106,6 +106,29 @@ const IncomeItem = ({
                 return ''
         }
     }
+
+    const expenseCatIcon = () => {
+        switch (category) {
+            case 'education':
+                return book;
+            case 'groceries':
+                return food;
+            case 'health':
+                return medical;
+            case 'subscriptions':
+                return tv;
+            case 'takeaways':
+                return takeaway;
+            case 'clothing':
+                return clothing;
+            case 'travelling':
+                return freelance;
+            case 'other':
+                return circle;
+            default:
+                return ''
+        }
+    }
   return (
     <IncomeItemStyled indicator={indicatorColor}>
         <div className="icon"></div>
