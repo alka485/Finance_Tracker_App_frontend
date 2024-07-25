1970f8021b2d995777698c00556709f6943e0a58
Alka Sah <alkasah485@gmail.com>
2024-07-24 22:15:03 -0700

    modification saved

--- src/components/IncomeItem.jsx
+++ src/components/IncomeItem.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import styled from 'styled-components'
 //------------------------------------------dependency
 import { bitcoin, book, calender, card, circle, clothing, comment, dollar, food, freelance, medical, money, piggy, stocks, takeaway, trash, tv, users, yt } from '../utils/Icons';
+import Button from './Button';
 
 
 const IncomeItemStyled = styled.div``;
@@ -25,7 +27,21 @@ const IncomeItem = ({
             <h5>{title}</h5>
             <div className="inner-content">
                 <div className="text">
-                    <p>{dollar}</p>
+                    <p>{dollar} {amount}</p>
+                    <p>{calender} {date}</p>
+                    <p>{comment} {description}</p>
+                </div>
+                <div className="btn-con">
+                    <Button>
+                    icon={trash}
+                            bPad={'1rem'}
+                            bRad={'50%'}
+                            bg={'var(--primary-color'}
+                            color={'#fff'}
+                            iColor={'#fff'}
+                            hColor={'var(--color-green)'}
+                            {/* onClick={() => deleteItem(id)} */}
+                    </Button>
                 </div>
             </div>
         </div>
