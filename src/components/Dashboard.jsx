7753f176ea9f937c34b9359277cb5b3d2f53116e
Alka Sah <alkasah485@gmail.com>
2024-07-26 15:27:03 -0700

    modification saved

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -29,21 +29,34 @@ const DashboardStyled = styled.div `
                         background: #FCF6F9;
                         border: 2px solid #FFFFFF;
                         box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
+                        border-radius: 20px;
+                        padding: 1rem;
                         p{
+                          font-size: 3.5rem;
+                          font-weight:700
                         }
-                  }    
-              }
-               .balance {
+                  } 
+                  .balance {
+                     grid-column: 2/4;
+                     display: flex;
+                     flex-direction:column;
+                     justify-content:center;
+                     align-items:center;
                     p{
-                    
-                    }
-               }   
+                      color: var(--color-green);
+                      opacity: 0.6;
+                      font-size: 4.5rem;
+                   }
+               }            
+             }   
           }
-     }
      .history-con {
          grid-column: 4/-1;
          h2{
+           margin: 1rem 0;
+           display: flex;
            align-items: center;
+           justify-content: space-between;
          }
          .salary-title{
          } 
