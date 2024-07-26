1f5560572cd19b4ff6032784b1cd98cd5009c2d6
Alka Sah <alkasah485@gmail.com>
2024-07-26 15:58:54 -0700

    modification saved

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,62 +10,33 @@ import {useGlobalContext}  from '../context/globalContext'
 import History from './History.jsx';
 
 const DashboardStyled = styled.div `
-     .stats-con{
-          display: grid;
-          grid-template-columns: repeat(5 , 1fr);
-          gap:2rem;
-          .chart-con{
-              grid-column: 1/4;
-              height: 400px;
-              .amount-con{
-                  display: grid;
-                  grid-template-columns: repeat(4,1fr);
-                  gap:2rem;
-                  margin-top: 2rem;
-                  .income, .expense{
-                      grid-column: span 2;
-                  }
-                  .income,.expense, .balance{
-                        background: #FCF6F9;
-                        border: 2px solid #FFFFFF;
-                        box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
-                        border-radius: 20px;
-                        padding: 1rem;
-                        p{
-                          font-size: 3.5rem;
-                          font-weight:700
-                        }
-                  } 
-                  .balance {
-                     grid-column: 2/4;
-                     display: flex;
-                     flex-direction:column;
-                     justify-content:center;
-                     align-items:center;
-                    p{
-                      color: var(--color-green);
-                      opacity: 0.6;
-                      font-size: 4.5rem;
-                   }
-               }            
-             }   
-          }
-     .history-con {
-         grid-column: 4/-1;
+      .stats-con{
+         display: grid;
+         grid-template-columns: repeat(5, 1fr);
+         gap: 2rem;
+
+          
+
+
+      }
+      .history-con{
+         grid-column: 4/-1;      
          h2{
            margin: 1rem 0;
-           display: flex;
+           display:flex;
            align-items: center;
            justify-content: space-between;
          }
          .salary-title{
-         } 
-         .salary-item{
-         } 
-     }     
-
-     
-
+            font-size: 1.2rem;
+            span{
+                font-size: 1.8rem;
+            }
+        }
+ 
+          
+      }    
+    
 `;
 
 const Dashboard = () => {
@@ -108,7 +79,6 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-        </div>
         <div className="history-con">
           {/* <History/> */}
           <h2 className='salary-title'>Min<span>Salary</span>Max</h2>
@@ -131,6 +101,7 @@ const Dashboard = () => {
               ${Math.max(...expenses.map(item => item.amount))}
             </p>
           </div>
+        </div>
         </div>
       </InnerLayout>
     </DashboardStyled>
