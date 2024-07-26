05088e42e96518f02b5cb21c5259472c6304dd66
Alka Sah <alkasah485@gmail.com>
2024-07-26 10:43:54 -0700

    modification saved

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -13,6 +13,9 @@ import {Chart as ChartJs,
 
 import {Line} from 'react-chartjs-2'
 import styled from 'styled-components'
+//---------------------------------------------------------dependency
+import {useGlobalContext} from '../context/globalContext'
+import  {dateFormat}  from '../utils/dateFormat'
 
 //Register the element
 
@@ -33,6 +36,14 @@ const ChartStyled = styled.div``;
 
 
 const Chart = () => {
+    const {incomes , expenses} = useGlobalContext()
+    const data = {
+        labels : incomes.map((inc) => {
+            const {date} = inc
+            return dateFormat(date) 
+        })
+
+    }
   return (
     <ChartStyled>
         <Line/>
