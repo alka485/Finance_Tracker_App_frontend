709a9f373c20fb9c765aa6d6086f4b5b9066df08
Alka Sah <alkasah485@gmail.com>
2024-07-26 11:58:41 -0700

    modification saved

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import styled from 'styled-components'
+//---------------------------------------------dependency
+import {useGlobalContext} from '../context/globalContext'
+
+const HistoryStyled = styled.div``;
 
 const History = () => {
+    const {transactionHistory} = useGlobalContext();
   return (
-    <div>History</div>
+    <HistoryStyled>History</HistoryStyled>
   )
 }
 
