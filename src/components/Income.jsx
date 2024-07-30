10eb31815dc65b8647c082790e425db033faa130
Alka Sah <alkasah485@gmail.com>
2024-07-30 08:59:04 -0700

    modification saved

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -52,7 +52,7 @@ const Income = () => {
     <IncomeStyled>
       <InnerLayout>
         <h1>Incomes</h1>
-        <h2 className='total-income'>Total Income: <span>${totalIncome()}</span></h2>
+        <h2 className='total-income'>Total Income: <span>${totalIncome().toFixed(2)}</span></h2>
         <div className="income-content">
           <div className="form-container">
             <Form/>
